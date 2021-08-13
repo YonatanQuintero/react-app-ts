8bfff52bbe22da228ee38545f3d81b47a67a1d1c
yonax73 <yonax73@gmail.com>
2021-08-12 20:06:43 -0500

    112 Hook useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './models/Todo';
 
 const App: React.FC = () =>{
-
+  const [todos,setTodos] = useState<Todo[]>([]);
   const AddTodo = (text:string) => {
-      console.log(text);
-  }
-
-  const todos = [
-    {id:"t1",text:"Completar el curso"},
-    {id:"t2",text:"Completar las evaluaciones"}
-  ];
+      setTodos([{id:Math.random().toString(),text:text}])
+  };
+  
 
   return(<div className="App">
     <NewTodo onAddTodo={AddTodo}/>
