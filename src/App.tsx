57c5e35d415531e32a5e82a99ce74aebc7ece6c2
yonax73 <yonax73@gmail.com>
2021-08-12 19:50:39 -0500

    111 - Comunicación entre componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+const App: React.FC = () =>{
+
+  const AddTodo = (text:string) => {
+      console.log(text);
+  }
+
+  const todos = [
+    {id:"t1",text:"Completar el curso"},
+    {id:"t2",text:"Completar las evaluaciones"}
+  ];
+
+  return(<div className="App">
+    <NewTodo onAddTodo={AddTodo}/>
+    <TodoList items={todos} />
+  </div>);
 }
 
 export default App;
