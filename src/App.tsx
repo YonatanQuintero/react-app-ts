b784481c1cdb9815c548250fdaf9b2d0b4f196b6
yonax73 <yonax73@gmail.com>
2021-08-15 18:30:16 -0500

    delete TODO

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,19 @@ const App: React.FC = () =>{
   const [todos,setTodos] = useState<Todo[]>([]);
 
   const AddTodo = (text:string) => {
-      setTodos(prevTodos =>[
+      setTodos(prevTodos => [
         ...prevTodos
         ,{id:Math.random().toString(),text:text}
       ])
   };
+
+  const todoDelete = (id:string) =>{
+    setTodos(prevTodos =>{return prevTodos.filter(todo=>todo.id !== id)});
+  }
   
   return(<div className="App">
     <NewTodo onAddTodo={AddTodo}/>
-    <TodoList items={todos} />
+    <TodoList items={todos} onDelete={todoDelete} />
   </div>);
 
 }
